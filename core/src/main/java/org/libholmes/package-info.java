85d94e3bcfa0165c095888eb1007e83d639c4db8
Graham Shaw <gdshaw@riscpkg.org>
2018-12-21 06:57:00 +0000

    Added package org.libholmes.

---
+++ core/src/main/java/org/libholmes/package-info.java
@@ -0,0 +1,13 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+/**
+ * This package contains core interfaces and classes for libholmes,
+ * a library for the forensic analysis of files and network traffic.
+ *
+ * Support for specific file formats and data protocols is provided
+ * within appropriately-named subpackages of this package.
+ */
+package org.libholmes;
