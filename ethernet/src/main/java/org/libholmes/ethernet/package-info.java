e2e402d084cd6044b612714925442e0641ff1026
Graham Shaw <gdshaw@riscpkg.org>
2018-12-21 19:53:31 +0000

    Added package org.libholmes.ethernet.

---
+++ ethernet/src/main/java/org/libholmes/ethernet/package-info.java
@@ -0,0 +1,10 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+/**
+ * This package provides support for analysing Ethernet network traffic
+ * as part of libholmes.
+ */
+package org.libholmes.ethernet;
