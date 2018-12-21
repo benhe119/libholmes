c2f6079823e076485ccc6e22cb1e5a367448cdec
Graham Shaw <gdshaw@riscpkg.org>
2018-12-21 13:34:47 +0000

    Added package org.libholmes.pcap.

---
+++ pcap/src/main/java/org/libholmes/pcap/package-info.java
@@ -0,0 +1,14 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+/**
+ * This package provides support for the PCAP file format as part of
+ * libholmes.
+ *
+ * Information about this format can be found at:
+ * - http://www.tcpdump.org/manpages/pcap-savefile.5.txt
+ * - https://wiki.wireshark.org/Development/LibpcapFileFormat
+ */
+package org.libholmes;
