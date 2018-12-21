ab135a18951ca8f0c776bee406afc41892908b5f
Graham Shaw <gdshaw@riscpkg.org>
2018-12-21 13:35:43 +0000

    Added class org.libholmes.pcap.PcapPacket.

---
+++ pcap/src/main/java/org/libholmes/pcap/PcapPacket.java
@@ -0,0 +1,92 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.libholmes.pcap;
+
+import org.libholmes.OctetReader;
+import org.libholmes.OctetString;
+
+/** A class to represent a packet within a PCAP file. */
+public class PcapPacket {
+    /** The seconds component of the timestamp.
+     * This is the number of seconds since the UNIX epoch. */
+    private final int tsSec;
+
+    /** The microseconds component of the timestamp.
+     * This is the number of microseconds since the start of the current
+     * second.
+     */
+    private final int tsUsec;
+
+    /** The captured length of this packet, in octets. */
+    private final int caplen;
+
+    /** The original length of this packet, in octets. */
+    private final int len;
+
+    /** The payload. */
+    private final OctetString payload;
+
+    /** Construct PcapPacket from octet source.
+     * @param reader the octet source
+     */
+    protected PcapPacket(OctetReader reader) {
+        this.tsSec = reader.readInt();
+        this.tsUsec = reader.readInt();
+        this.caplen = reader.readInt();
+        this.len = reader.readInt();
+        this.payload = reader.readOctetString(getCapturedLength());
+    }
+
+    /** Get the seconds component of the timestamp.
+     * This is the number of seconds since the UNIX epoch.
+     * @return the seconds component
+     */
+    public final int getTsSec() {
+        return tsSec;
+    }
+
+    /** Get the microseconds component of the timestamp.
+     * This is the number of microseconds since the start of the current
+     * second.
+     * @return the microseconds component
+     */
+    public final int getTsUsec() {
+        return tsUsec;
+    }
+
+    /** Get the captured length of this packet.
+     * @return the captured length, in octets
+     */
+    public final int getCapturedLength() {
+        return caplen;
+    }
+
+    /** Get the original length of this packet.
+     * @return the original length, in octets
+     */
+    public final int getOriginalLength() {
+        return len;
+    }
+
+    /** Get the payload.
+     * Note that the payload may have been truncated during capture, as the
+     * number of octets recorded is limited by the snaplen parameter. This
+     * condition can be detected by comparing the values returned by
+     * getCapturedLength() and getOriginalLength().
+     * @return the payload
+     */
+    public final OctetString getPayload() {
+        return payload;
+    }
+
+    /** Parse PcapPacket from octet source.
+     * @param reader the octet source
+     * @return the resulting PcapPacket
+     */
+    public static PcapPacket parse(OctetReader reader) {
+        return new PcapPacket(reader);
+    }
+}
