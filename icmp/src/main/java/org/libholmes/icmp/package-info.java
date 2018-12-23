1f5c7a9c6e92e4c38b7c6b9c8e30da74da81effc
Graham Shaw <gdshaw@riscpkg.org>
2018-12-23 11:50:04 +0000

    Added package org.libholmes.icmp.

---
+++ icmp/src/main/java/org/libholmes/icmp/package-info.java
@@ -0,0 +1,10 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+/**
+ * This is a subpackage of libholmes. It provides classes for the forensic
+ * analysis of Internet Control Message Protocol (ICMP) network traffic.
+ */
+package org.libholmes.icmp;
