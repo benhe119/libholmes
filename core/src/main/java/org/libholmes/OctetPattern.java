4edb000aec6fc7b44c5df37c04ee0b2c7041df91
Graham Shaw <gdshaw@riscpkg.org>
2018-12-24 11:51:05 +0000

    Added class org.libholmes.OctetPattern.

---
+++ core/src/main/java/org/libholmes/OctetPattern.java
@@ -0,0 +1,28 @@
+// This file is part of libholmes.
+// Copyright 2018 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.libholmes;
+
+import javax.json.JsonValue;
+
+/** An abstract base class to represent a pattern for matching against a
+ * sequence of octets.
+ */
+public abstract class OctetPattern {
+    /** Determine whether this pattern matches a given sequence of octets.
+     * @param octets the octet sequence to be matched
+     * @return true if the whole octet sequence matched, otherwise false
+     */
+    public abstract boolean match(OctetReader octets);
+
+    /** Parse OctetPattern from a specification in JSON format.
+     * @param jsonSpec the specification to be parsed
+     * @return the resulting OctetPattern
+     */
+    public static OctetPattern parse(JsonValue jsonSpec) {
+        throw new IllegalArgumentException(
+            "invalid specification for OctetPattern");
+    }
+}
