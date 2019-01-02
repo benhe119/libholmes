e5d12890916a4f7bb614655d6023c0c9e5646d7c
Graham Shaw <gdshaw@riscpkg.org>
2019-01-02 23:18:51 +0000

    Added repeat and minLength arguments to HexOctetPattern specification.

--- core/src/main/java/org/libholmes/HexOctetPattern.java
+++ core/src/main/java/org/libholmes/HexOctetPattern.java
@@ -12,18 +12,40 @@ public class HexOctetPattern extends OctetPattern {
     /** The pattern to be matched. */
     private final HexOctetString pattern;
 
+    /** True if this is a repeating pattern, otherwise false. */
+    private final boolean repeat;
+
+    /** The minimum permitted length, in octets. */
+    private final int minLength;
+
     /** Parse HexOctetPattern from a specification in JSON format.
      * @param jsonSpec the specification to be parsed
      */
     public HexOctetPattern(JsonObject jsonSpec) {
         pattern = new HexOctetString(jsonSpec.getString("content"));
+        repeat = jsonSpec.getBoolean("repeat", false);
+        minLength = jsonSpec.getInt("minLength", 0);
     }
 
     @Override
     public final boolean matches(OctetReader reader) {
-        if (reader.remaining() < pattern.length()) {
-            return false;
+        if (repeat) {
+            int index = 0;
+            while (reader.hasRemaining()) {
+                if (reader.readByte() != pattern.getByte(index)) {
+                    return false;
+                }
+                index += 1;
+                if (index == pattern.length()) {
+                    index = 0;
+                }
+            }
+            return index >= minLength;
+        } else {
+            if (reader.remaining() < pattern.length()) {
+                return false;
+            }
+            return reader.readOctetString(pattern.length()).equals(pattern);
         }
-        return reader.readOctetString(pattern.length()).equals(pattern);
     }
 }
