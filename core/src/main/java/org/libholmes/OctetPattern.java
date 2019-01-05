4d48a54b611a795ad1b53d27b7a62cfba23558ff
Graham Shaw <gdshaw@riscpkg.org>
2019-01-05 20:39:35 +0000

    Modified OctetPattern to construct named pattern types using reflection. This will allow pattern types to be registered which are not known to libholmes-core at compile time (and which would otherwise result in a circular dependency).

--- core/src/main/java/org/libholmes/OctetPattern.java
+++ core/src/main/java/org/libholmes/OctetPattern.java
@@ -1,9 +1,12 @@
 // This file is part of libholmes.
-// Copyright 2018 Graham Shaw.
+// Copyright 2018-2019 Graham Shaw.
 // Distribution and modification are permitted within the terms of the
 // GNU General Public License (version 3 or any later version).
 
 package org.libholmes;
+
+import java.lang.reflect.Constructor;
+import java.util.HashMap;
 
 import javax.json.JsonValue;
 import javax.json.JsonObject;
@@ -13,6 +16,39 @@ import javax.json.JsonArray;
  * sequence of octets.
  */
 public abstract class OctetPattern {
+    /** The registered constructors, indexed by pattern type name. */
+    private static final HashMap<String, Constructor> types =
+        new HashMap<String, Constructor>();
+
+    /** Register an OctetPattern class.
+     * @param typeName the name of the pattern type to be registered
+     * @param className the name of the corresponding Java class
+     */
+    private static void registerType(String typeName, String className) {
+        Constructor constructor = null;
+        try {
+            Class classObj = Class.forName(className);
+            constructor = classObj.getConstructor(
+                new Class[]{JsonObject.class});
+        } catch (ClassNotFoundException ex) {
+            // No action: will be registered with null value.
+        } catch (ReflectiveOperationException ex) {
+            throw new RuntimeException(ex);
+        }
+        types.put(typeName, constructor);
+    }
+
+    static {
+        registerType("hex", "org.libholmes.HexOctetPattern");
+        registerType("wildcard", "org.libholmes.WildcardOctetPattern");
+        registerType("text", "org.libholmes.TextOctetPattern");
+        registerType("timeval", "org.libholmes.TimevalOctetPattern");
+        registerType("time", "org.libholmes.TimeOctetPattern");
+        registerType("step", "org.libholmes.StepOctetPattern");
+        registerType("domain", "org.libholmes.DomainOctetPattern");
+        registerType("inet", "org.libholmes.inet.InetAddressOctetPattern");
+    }
+
     /** Determine whether this pattern matches a given sequence of octets.
      * If it is able to, this method will reade a maximal-length sequence of
      * octets from the given reader which are an exact match for the pattern.
@@ -33,24 +69,22 @@ public abstract class OctetPattern {
             return new SequenceOctetPattern(jsonArray);
         } else if (jsonSpec instanceof JsonObject) {
             JsonObject jsonObject = (JsonObject) jsonSpec;
-            String type = jsonObject.getString("type");
-            if (type.equals("hex")) {
-                return new HexOctetPattern(jsonObject);
-            } else if (type.equals("wildcard")) {
-                return new WildcardOctetPattern(jsonObject);
-            } else if (type.equals("text")) {
-                return new TextOctetPattern(jsonObject);
-            } else if (type.equals("domain")) {
-                return new DomainOctetPattern(jsonObject);
-            } else if (type.equals("timeval")) {
-                return new TimevalOctetPattern(jsonObject);
-            } else if (type.equals("time")) {
-                return new TimeOctetPattern(jsonObject);
-            } else if (type.equals("step")) {
-                return new StepOctetPattern(jsonObject);
+            String typeName = jsonObject.getString("type");
+            Constructor constructor = types.get(typeName);
+            if (constructor != null) {
+                try {
+                    return (OctetPattern) constructor.newInstance(jsonSpec);
+                } catch (ReflectiveOperationException ex) {
+                    throw new RuntimeException(ex);
+                }
+            } else if (types.containsKey(typeName)) {
+                throw new IllegalArgumentException(String.format(
+                    "Pattern type %s not available", typeName));
+            } else {
+                throw new IllegalArgumentException(String.format(
+                    "Pattern type %s not recognised", typeName));
             }
         }
-        throw new IllegalArgumentException(
-            "invalid specification for OctetPattern");
+        throw new IllegalArgumentException("Invalid pattern type");
     }
 }
