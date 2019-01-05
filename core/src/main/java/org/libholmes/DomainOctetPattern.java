d14f2d118b22c553ba45d3ab96995561dfc0a593
Graham Shaw <gdshaw@riscpkg.org>
2019-01-05 23:29:11 +0000

    Changed DomainOctetPattern to use context.

--- core/src/main/java/org/libholmes/DomainOctetPattern.java
+++ core/src/main/java/org/libholmes/DomainOctetPattern.java
@@ -24,53 +24,32 @@ public class DomainOctetPattern extends OctetPattern {
 
         int index = 0;
         int limit = reader.remaining();
-        int labels = 0;
+        int labels = 1;
 
         StringBuilder domainBuilder = new StringBuilder();
         while (index < limit) {
-            if (labels != 0) {
-                if (index < limit) {
-                    char c = (char)reader.peekByte(index);
-                    if (c == '.') {
-                        index += 1;
-                    } else {
-                        break;
-                    }
-                } else {
-                    break;
+            char c = (char)reader.peekByte(index);
+            if (((c >= '0') && (c <= '9')) ||
+                ((c >= 'A') && (c <= 'Z')) ||
+                ((c >= 'a') && (c <= 'z')) ||
+                (c == '-')) {
+
+                domainBuilder.append(c);
+            } else if (c == '.') {
+                domainBuilder.append(c);
+                labels += 1;
+            } else {
+                return false;
+            }
+            index += 1;
+
+            if (labels > 1) {
+                if (context.isHostIdentifier(domainBuilder.toString())) {
+                    reader.skip(index);
+                    return true;
                 }
             }
-
-            StringBuilder labelBuilder = new StringBuilder();
-            while (index < limit) {
-                char c = (char)reader.peekByte(index);
-                if (((c >= '0') && (c <= '9')) ||
-                    ((c >= 'A') && (c <= 'Z')) ||
-                    ((c >= 'a') && (c <= 'z')) ||
-                    (c == '-')) {
-
-                    labelBuilder.append(c);
-                    index += 1;
-                } else {
-                    break;
-                }
-            }
-
-            String label = labelBuilder.toString();
-            if (label.length() == 0) {
-                break;
-            }
-
-            if (domainBuilder.length() != 0) {
-                domainBuilder.append(".");
-            }
-            domainBuilder.append(label);
-            labels += 1;
         }
-        if (labels < 2) {
-            return false;
-        }
-        reader.skip(index);
-        return true;
+        return false;
     }
 }
