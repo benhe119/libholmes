c0947483b344d33aafc700a11b06af84bd1cf681
Graham Shaw <gdshaw@riscpkg.org>
2019-01-06 19:52:27 +0000

    Added class org.libholmes.tcp.TcpOption.

---
+++ tcp/src/main/java/org/libholmes/tcp/TcpOption.java
@@ -0,0 +1,92 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+package org.libholmes.tcp;
+
+import javax.json.JsonObjectBuilder;
+
+import org.libholmes.OctetReader;
+import org.libholmes.OctetString;
+import org.libholmes.Artefact;
+import org.libholmes.ParseException;
+
+/** A class to represent a TCP option. */
+public class TcpOption extends Artefact {
+    /** The kind of this option. */
+    private final int kind;
+
+    /** The payload. */
+    private final OctetString payload;
+
+    /** Copy-construct TCP option from an existing option.
+     * @param that the existing option
+     */
+    TcpOption(TcpOption that) {
+        super(that.getParent());
+        this.kind = that.kind;
+        this.payload = that.payload;
+    }
+
+    /** Parse TCP option from an OctetReader.
+     * @param parent the parent artefact, or null if none
+     * @param reader the OctetReader to be parsed
+     * @throws ParseException if the octet sequence cannot be parsed
+     */
+    TcpOption(Artefact parent, OctetReader reader) throws ParseException {
+        super(parent);
+        this.kind = reader.readByte() & 0xff;
+        if (this.kind >= 2) {
+            int length = reader.readByte() & 0xff;
+            if (length < 2) {
+                throw new ParseException("invalid TCP option length");
+            }
+            this.payload = reader.readOctetString(length - 2);
+        } else {
+            this.payload = reader.readOctetString(0);
+        }
+    }
+
+    /** Get the kind of this option.
+     * @return the option kind
+     */
+    public final int getKind() {
+        return kind;
+    }
+
+    /** Get the length of this option.
+     * This includes the option kind and (if present) length fields.
+     * @return the length, in octets
+     */
+    public final int getLength() {
+        return (kind >= 2) ? payload.length() + 2 : 1;
+    }
+
+    /** Get the payload for this option.
+     * This does not include the option kind or length fields.
+     * @return the payload
+     */
+    public final OctetString getPayload() {
+        return payload;
+    }
+
+    @Override
+    protected void buildJson(JsonObjectBuilder builder) {
+        builder.add("kind", kind);
+        builder.add("length", getLength());
+        builder.add("payload", payload.toString());
+    }
+
+    /** Parse TCP option.
+     * @param parent the parent artefact, or null if none
+     * @param reader the octet source to be parsed
+     * @return the resulting TCP option
+     * @throws ParseException if the octet sequence cannot be parsed
+     */
+    static TcpOption parse(Artefact parent, OctetReader reader)
+        throws ParseException {
+
+        throw new ParseException("Not yet implemented");
+    }
+}
