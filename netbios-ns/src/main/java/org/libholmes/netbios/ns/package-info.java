24b55d0b667d5f21c7712b22c64e3d4fb1212561
Graham Shaw <gdshaw@riscpkg.org>
2019-02-23 10:53:54 +0000

    Added package org.libholmes.netbios.ns.

---
+++ netbios-ns/src/main/java/org/libholmes/netbios/ns/package-info.java
@@ -0,0 +1,10 @@
+// This file is part of libholmes.
+// Copyright 2019 Graham Shaw.
+// Distribution and modification are permitted within the terms of the
+// GNU General Public License (version 3 or any later version).
+
+/**
+ * This is a subpackage of libholmes. It provides classes for the forensic
+ * analysis of NetBIOS Name Service network traffic.
+ */
+package org.libholmes.netbios.ns;
